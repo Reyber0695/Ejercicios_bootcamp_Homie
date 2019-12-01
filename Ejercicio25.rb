9a5dbf2ca35db514a7cc946616e10a51f7d9d3bd
Reyber0695 <57369477+Reyber0695@users.noreply.github.com>
2019-11-30 23:34:21 -0600

    Delete Ejercicio25.rb

--- Ejercicio25.rb
+++
@@ -1,8 +0,0 @@
-def calculate(number_one, number_two)
-  yield(number_one, number_two)
-end
-  
-calculate(15, 20) {|number_one, number_two| puts number_one + number_two}
-calculate(15, 20) {|number_one, number_two| puts number_one - number_two}
-calculate(15, 20) {|number_one, number_two| puts number_one * number_two}
-calculate(15, 20) {|number_one, number_two| puts number_one / number_two}
